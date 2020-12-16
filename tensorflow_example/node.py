99cffa4f644425b581c4fd9b58cf3a3a841b04c2
sangheonEN <jteks6@gmail.com>
2020-12-16 11:07:04 +0900

    node
    node

--- tensorflow_example/node.py
+++ tensorflow_example/node.py
@@ -1,13 +1,20 @@
 import tensorflow as tf
 
-# 앞쪽 노드 빌드
+# Graph Build (node와 function 정의)
 node1 = tf.constant(3.0, tf.float32)
 node2 = tf.constant(4.0)
 node3 = tf.add(node1, node2)
-print(f"node1 = {node1}, node2 = {node2}, node3 = {node3}")
-
-# 뒤쪽 노드 함수로 정의
-
-
+# print(f"node1 = {node1}, node2 = {node2}, node3 = {node3}")
 
 # 뒤쪽 노드 출력
+sess = tf.Session()
+print("sess.run(node1, node2): {}".format(sess.run([node1, node2])))
+print("sess.run(node3): {}".format(sess.run([node3])))
+
+# Placeholder
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b
+
+print(sess.run(adder_node, feed_dict={a: 3, b : 4.5}))
+print(sess.run(adder_node, feed_dict={a: [1,3], b : [3.5, 4.5]}))
